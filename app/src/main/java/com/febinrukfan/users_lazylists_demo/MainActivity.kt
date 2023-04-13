a36ee5529fd126eea303783762dbf72e6f8feb9c
Febin Rukfan <32138498+FebinRukfan@users.noreply.github.com>
2023-04-13 00:05:39 -0400

    ADDED lazy column with 100 user rows

--- app/src/main/java/com/febinrukfan/users_lazylists_demo/MainActivity.kt
+++ app/src/main/java/com/febinrukfan/users_lazylists_demo/MainActivity.kt
@@ -3,30 +3,54 @@ package com.febinrukfan.users_lazylists_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.febinrukfan.users_lazylists_demo.ui.theme.UserslazylistsdemoTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                usersList()
+            UsersList()
         }
     }
 }
 
 @Composable
-fun usersList() {
+fun UsersList() {
+
+    var listItem = remember {(1..100).map { "User  $it" }}
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+
+        ){
+            items(listItem){ item->
+                Row (
+                    modifier = Modifier.fillMaxWidth())
+                {
+                    Text (text = item)
+                }
+
+            }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    usersList()
+    UsersList()
 }
