e7559f4f09cde362e5617b5ac3f8a833ff645902
Febin Rukfan <32138498+FebinRukfan@users.noreply.github.com>
2023-08-16 19:52:52 -0400

    ADDED users data class with requirements, user name items and deletes data on item click.

--- app/src/main/java/com/febinrukfan/users_lazylists_demo/MainActivity.kt
+++ app/src/main/java/com/febinrukfan/users_lazylists_demo/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.febinrukfan.users_lazylists_demo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -19,9 +24,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -30,6 +37,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.febinrukfan.users_lazylists_demo.data.user
+import com.febinrukfan.users_lazylists_demo.data.users
 import com.febinrukfan.users_lazylists_demo.ui.theme.UserslazylistsdemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,24 +63,31 @@ fun usersListApplication() {
             Surface(shadowElevation = 3.dp) {
                 SmallTopAppBar(
                     title = { Text(
-                        text = stringResource(id = R.string.users),
-                    ) })
+                        text = stringResource(id = R.string.users)
+                    ) },
+                    colors = TopAppBarDefaults.smallTopAppBarColors())
             }
         }
         ) { paddingValues ->
-        var listItem = remember {(1..100).map { "User  $it" }}
+
+        val users = remember {
+            mutableStateListOf(*user.toTypedArray())
+        }
+        val context = LocalContext.current
 
         LazyColumn(
             contentPadding = paddingValues,
             verticalArrangement = Arrangement.spacedBy(8.dp)
 
         ){
-            items(listItem){ item->
-                Row (
-                    modifier = Modifier.fillMaxWidth())
-                {
-                    Text (text = item)
+            itemsIndexed(users){ index, user->
+
+                UserItem(user = user, index = index){
+                    users.removeAt(index)
+                    Toast.makeText(context, "User Deleted: ${user.userId}", Toast.LENGTH_SHORT).show()
                 }
+
+
 
             }
 
@@ -81,6 +97,27 @@ fun usersListApplication() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserItem(modifier: Modifier = Modifier, user: users, index: Int, onItemClick: () -> Unit) {
+    ElevatedCard(
+        modifier = modifier
+            .clickable { onItemClick() }
+            .padding(horizontal = 12.dp, vertical = 8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(top = 8.dp), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(modifier = Modifier, text = stringResource(id = R.string.user_id) + " ${user.userId}")
+            Text(modifier = Modifier, text = stringResource(id = R.string.username) + " ${user.username}")
+        }
+
+    }
+}
 
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/febinrukfan/users_lazylists_demo/data/users.kt
@@ -0,0 +1,51 @@
+package com.febinrukfan.users_lazylists_demo.data
+
+import androidx.compose.ui.text.capitalize
+import kotlin.random.Random
+
+data class users(
+    val userId : Long,
+    val username : String,
+    val fullName : String,
+    val email : String)
+
+val emailDomains = listOf("@gmail.com", "@yahoo.com", "@hotmail.com", "@outlook.com")
+
+val generator = generateSequence {
+    users(
+        //userId must be long and of 8 digit,
+        userId = Random.nextLong(100_000_00L, 999_999_99L),
+        //username must be alphanumeric and 6 characters,
+        username = generateUsernames(6),
+        //fullName must be alphabetical and up to 20 characters
+        fullName = generateFullNames(20),
+        //email should be any valid email address.
+        email = "${generateUsernames(6)}${emailDomains.random()}"
+    )
+}
+
+fun generateUsernames(limit: Int): String {
+    val nameLength = Random.nextInt(1, limit + 1)
+    val nameBuilder = StringBuilder(nameLength)
+    repeat(nameLength) {
+        val letter = ('a'..'z').filter { it.isLetter() }.random()
+        nameBuilder.append(letter)
+    }
+    return nameBuilder.toString()
+}
+
+fun generateFullNames(limit: Int): String {
+    val usernameBuilder = StringBuilder(limit)
+    repeat(limit) {
+        val char = when (Random.nextInt(3)) {
+            0 -> ('a'..'z')
+            1 -> ('A'..'Z')
+            else -> ('0'..'9')
+        }.filter { it.isLetterOrDigit() }.random()
+        usernameBuilder.append(char)
+    }
+    return usernameBuilder.toString()
+}
+
+
+val user = generator.take(100).toList().sortedBy { it.fullName }
